8d4ac6f39b9f5935fd19b8fcdd71e46c3ac1bfe7
simon <srehbach@gmail.com>
2019-05-05 19:22:46 +0200

    Update install.sh
    small fixes

--- install.sh
+++ install.sh
@@ -8,21 +8,19 @@
 # 1) Install clean raspberry with stretch FULL (remember wpa_supplicant and ssh)
 # 2) Update: sudo apt-get update && sudo apt-get upgrade; raspi-config to set GPU memory to 256 (under advanced options); set hostname, timezone, locales
 
-## Get git and omxplayer and flask installed - usually already there
+## Get git and omxplayer installed - usually already there
 sudo apt-get -y install git omxplayer
-sudo pip install flask
 
 ## Get the screenpi API script installed
-mkdir /home/pi/screenpi
-cd /home/pi/screenpi
+cd /home/pi
 git clone https://github.com/dpsimon/screenpi
-cp screen-api*.* /home/pi/screenpi
 
 # modify screen config
 sudo mv /boot/config.txt /boot/config.txt.backup
 sudo cp config.txt /boot
 sudo mv /etc/X11/xorg.conf.d/99-calibration.conf /etc/X11/xorg.conf.d/99-calibration.conf.backup
-sudo cp 99-calibration /etc/X11/xorg.conf.d/99-calibration.conf
+mkdir /etc/X11/xorg.conf.d
+sudo cp 99-calibration.conf /etc/X11/xorg.conf.d/99-calibration.conf
 
 # modify cmdline.txt so it has consoleblank=15 to blank the console after a few secs
 sudo mv /boot/cmdline.txt /boot/cmdline.txt.backup
